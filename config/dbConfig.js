3ade2a80f96ae3f74c4a15b3a324b5f5ccddac7f
HoangPham1208 <hoang.pham1208@hcmut.edu.vn>
2024-04-04 03:05:22 +0700

    setup mysql

---
+++ config/dbConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+  host: 'localhost',
+  user: 'root',
+  password: '123',
+  database: 'dahttt',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+};
---
+++ database/dbConnect.js
@@ -0,0 +1,22 @@
+const dbConfig = require('../config/dbConfig')
+const mysql = require('mysql2')
+const db = mysql.createConnection({
+    host: dbConfig.host,
+    user: dbConfig.user,
+    password: dbConfig.password,
+    database: dbConfig.database,
+    waitForConnections: dbConfig.waitForConnections,
+    connectionLimit: dbConfig.connectionLimit,
+    queueLimit: dbConfig.queueLimit,
+})
+
+db.connect((error)=>{
+    if (error) {
+        console.log("Failed to connect to MySQL database ",dbConfig.database," : ",error)
+    }
+    else {
+        console.log('Connected to MySQL database', dbConfig.database)
+    }
+})
+
+module.exports = db
