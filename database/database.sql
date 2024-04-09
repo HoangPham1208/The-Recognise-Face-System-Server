d2fce0e10af0bf6dfbd35c0e75a29f4fabea57fd
Sanuicay <denras2102@gmail.com>
2024-04-09 22:47:14 +0700

    added procedure

--- database/database.sql
+++ database/database.sql
@@ -64,14 +64,12 @@ ALTER TABLE `staff`
 ALTER TABLE `staff`
     ADD CONSTRAINT `staff_FK_1` FOREIGN KEY (`ID`) REFERENCES `account` (`ID`);
 
-
-
 -- --------------------------------------------------------
 -- to be continued
 -- --------------------------------------------------------
 -- Insert some data into the table `account`
 INSERT INTO `account` (`ID`, `account_name`, `password`, `avatar`, `name`, `phone_num`, `status`, `address`, `face_fodel`) VALUES
-(`0000000001`, `user_1`, `123`, `https://media.istockphoto.com/id/1300845620/vi/vec-to/bi%E1%BB%83u-t%C6%B0%E1%BB%A3ng-ng%C6%B0%E1%BB%9Di-d%C3%B9ng-ph%E1%BA%B3ng-b%E1%BB%8B-c%C3%B4-l%E1%BA%ADp-tr%C3%AAn-n%E1%BB%81n-tr%E1%BA%AFng-bi%E1%BB%83u-t%C6%B0%E1%BB%A3ng-ng%C6%B0%E1%BB%9Di-d%C3%B9ng-minh-h%E1%BB%8Da-vector.webp?s=1024x1024&w=is&k=20&c=cJE9q0XBXMumQRNYHSfUGEsDQEHhOit5jAioPbVKIjI=`, `Le Van A`, `0123456789`, `0`, `Ha Noi`, `https://i.pinimg.com/474x/7e/cb/87/7ecb873df3c969d8fce9c915e59233b1.jpg`);
+(0000000001, 'user_1', '123', 'https://media.istockphoto.com/id/1300845620/vi/vec-to/bi%E1%BB%83u-t%C6%B0%E1%BB%A3ng-ng%C6%B0%E1%BB%9Di-d%C3%B9ng-ph%E1%BA%B3ng-b%E1%BB%8B-c%C3%B4-l%E1%BA%ADp-tr%C3%AAn-n%E1%BB%81n-tr%E1%BA%AFng-bi%E1%BB%83u-t%C6%B0%E1%BB%A3ng-ng%C6%B0%E1%BB%9Di-d%C3%B9ng-minh-h%E1%BB%8Da-vector.webp?s=1024x1024&w=is&k=20&c=cJE9q0XBXMumQRNYHSfUGEsDQEHhOit5jAioPbVKIjI=', 'Le Van A', '0123456789', '0', 'Ha Noi', 'https://i.pinimg.com/474x/7e/cb/87/7ecb873df3c969d8fce9c915e59233b1.jpg');
 
 
 -- Procedure `add_employee`:
