c76886e1810a235ed4bfe57996b7e43c1bbd039e
HoangPham1208 <hoang.pham1208@hcmut.edu.vn>
2024-04-11 02:54:01 +0700

    sse.js tracking

--- src/sse.js
+++ src/sse.js
@@ -1,37 +1,18 @@
-var createError = require("http-errors");
 var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var cors = require("cors");
-var bodyParser = require("body-parser");
+var sse = express.Router();
 
-var Router = require("./routes/index");
-var { sse, setFlag } = require("./sse");
-// var myIoT = require('./iot_mqtt')
-var app = express();
-// myIoT.setup() // setup MQTT and IoT connect to backend
+var flag = true;
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// Route in routes
-Router(app);
-app.use(sse);
-//---------------------------------------------------------------------------------------
-
-app.get("/flag", (req, res) => {
-  setFlag()
+sse.get("/progress", (req, res) => {
+  res.set("Content-Type", "text/event-stream");
+  let progress = 0;
+  setInterval(() => {
+    // runs every 2 seconds
+    res.write(String(flag));
+  }, 2000);
 });
+function setFlag(){
+    flag = !flag
+}
 
-const port = 3001;
-
-app.listen(port, () => {
-  console.log(`Our server is running on port ${port}`);
-});
+module.exports = { sse, setFlag };
