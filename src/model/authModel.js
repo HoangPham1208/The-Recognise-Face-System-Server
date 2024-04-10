f435a40252cbd49021529925402b9a14875694bd
HoangPham1208 <hoang.pham1208@hcmut.edu.vn>
2024-04-10 14:56:45 +0700

    JWT setup

--- src/model/authModel.js
+++ src/model/authModel.js
@@ -1,14 +1,20 @@
 const db = require("../database/dbConnect");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
-  login: async (username, password) => {
+  login: async (account_name, password) => {
     return new Promise((resolve, reject) => {
-      let sql = `SELECT * FROM user WHERE userName = ? and passWord = ?`;
-      let params = [username, password];
+      let sql = `SELECT * FROM account WHERE account_name = ? and password = ?`;
+      let params = [account_name, password];
       db.query(sql, params, (error, result) => {
-        if (error) reject(error)
-        if (result.length) resolve(true)
-        else resolve(false)
+        if (error) reject(error);
+        if (result.length) {
+          const id = result[0].ID;
+          const token = jwt.sign({ id }, process.env.SECRET_TOKEN, {
+            expiresIn: "24h",
+          });
+          resolve(token);
+        } else resolve(false);
       });
     });
   },
