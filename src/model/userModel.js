f643b338cb24401e72866b5f6e434f8a3e45872b
Clay <tadinhtien5@gmail.com>
2024-04-30 00:41:43 +0700

    fix updateInfo

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -1,79 +1,79 @@
-const db = require("../database/dbConnect");
+const db = require('../database/dbConnect')
 
-const updateUser = async (account_ID, phone_num, address, email, avatar) => {
+const updateInfo = async (account_ID, phone_num, address, email, avatar) => {
   return new Promise((resolve, reject) => {
-    const sql = `CALL update_info(?,?,?,?,?)`;
-    const params = [account_ID, phone_num, address, email, avatar];
+    const sql = `CALL update_info(?,?,?,?,?)`
+    const params = [account_ID, phone_num, address, email, avatar]
     db.query(sql, params, (err, result) => {
-      if (err) reject(err);
-      resolve(true);
-    });
-  });
-};
+      if (err) reject(err)
+      resolve(true)
+    })
+  })
+}
 
 const changePassword = async (account_ID, hash_password) => {
   return new Promise((resolve, reject) => {
     const sql = `
     UPDATE account
     SET password = ?
-    WHERE ID = ?`;
-    const params = [account_ID, hash_password];
+    WHERE ID = ?`
+    const params = [account_ID, hash_password]
     db.query(sql, params, (err, result) => {
-      if (err) reject(err);
-      resolve(true);
-    });
-  });
-};
+      if (err) reject(err)
+      resolve(true)
+    })
+  })
+}
 
 const getUser = async (account_name) => {
   return new Promise((resolve, reject) => {
-    const sql = `SELECT * FROM account WHERE account_name = ?`;
-    const params = [account_name];
+    const sql = `SELECT * FROM account WHERE account_name = ?`
+    const params = [account_name]
     db.query(sql, params, (err, result) => {
-      if (err) reject(err);
+      if (err) reject(err)
       if (result.length) {
-        resolve(result[0]);
+        resolve(result[0])
       } else {
-        resolve(false);
+        resolve(false)
       }
-    });
-  });
-};
+    })
+  })
+}
 const createOtp = async (account_ID) => {
   return new Promise((resolve, reject) => {
-    const sql = `CALL create_OTP(?)`;
-    const params = [account_ID];
+    const sql = `CALL create_OTP(?)`
+    const params = [account_ID]
     db.query(sql, params, (err, result) => {
-      if (err) reject(err);
-      resolve(result);
-    });
-  });
-};
+      if (err) reject(err)
+      resolve(result)
+    })
+  })
+}
 const deleteOtp = async (account_ID) => {
   return new Promise((resolve, reject) => {
-    const sql = `CALL delete_OTP(?)`;
-    const params = [account_ID];
+    const sql = `CALL delete_OTP(?)`
+    const params = [account_ID]
     db.query(sql, params, (err, result) => {
-      if (err) reject(err);
+      if (err) reject(err)
 
-      resolve(true);
-    });
-  });
-};
+      resolve(true)
+    })
+  })
+}
 const getOtp = async (account_ID) => {
   return new Promise((resolve, reject) => {
-    const sql = `SELECT code FROM OTP WHERE account_ID = ?`;
-    const params = [account_ID];
+    const sql = `SELECT code FROM OTP WHERE account_ID = ?`
+    const params = [account_ID]
     db.query(sql, params, (err, result) => {
-      if (err) reject(err);
+      if (err) reject(err)
       if (result.length) {
-        resolve(result[0]);
+        resolve(result[0])
       } else {
-        resolve(false);
+        resolve(false)
       }
-    });
-  });
-};
+    })
+  })
+}
 
 // PENDING model
 const getAttendanceTrack = async (account_ID) => {
@@ -81,73 +81,73 @@ const getAttendanceTrack = async (account_ID) => {
     const sql = `
     SELECT e.position, t.status as 'Status of Date', idata.date, idata.time, idata.value, idata.type, idev.name, idev.location, idev.role FROM employee as e, tracking_work_days as t, iot_data as idata, iot_device as idev
     WHERE e.ID = t.employee_ID and t.employee_ID = idata.employee_ID and idata.device_ID = idev.ID and e.ID = ?
-    `;
-    const params = [account_ID];
+    `
+    const params = [account_ID]
     db.query(sql, params, (err, result) => {
-      if (err) reject(err);
+      if (err) reject(err)
       if (result.length) {
-        resolve(result);
+        resolve(result)
       } else {
-        resolve(false);
+        resolve(false)
       }
-    });
-  });
-};
+    })
+  })
+}
 
 const getEmployeeDetails = async (account_ID) => {
   return new Promise((resolve, reject) => {
     const sql = `
     SELECT avatar,name,phone_num,address, position, working_days from account, employee
     where account.ID = employee.ID and account.id = ?
-    `;
-    const params = [account_ID];
+    `
+    const params = [account_ID]
     db.query(sql, params, (err, result) => {
-      if (err) reject(err);
+      if (err) reject(err)
       if (result.length) {
-        resolve(result[0]);
+        resolve(result[0])
       } else {
-        resolve(false);
+        resolve(false)
       }
-    });
-  });
-};
+    })
+  })
+}
 
 const sendForm = async (account_ID, date_time, description) => {
   return new Promise((resolve, reject) => {
-    const sql = `SELECT * from announcement`;
-    const params = [account_ID, date_time, description];
+    const sql = `SELECT * from announcement`
+    const params = [account_ID, date_time, description]
     db.query(sql, params, (err, result) => {
-      if (err) reject(err);
-      else resolve(true);
-    });
-  });
-};
+      if (err) reject(err)
+      else resolve(true)
+    })
+  })
+}
 
 const getForm = async (account_ID) => {
   return new Promise((resolve, reject) => {
-    const sql = `SELECT * from form as f, request as r where f.form_ID = request.form_ID and request.staff_ID = ?`;
-    const params = [account_ID];
+    const sql = `SELECT * from form as f, request as r where f.form_ID = request.form_ID and request.staff_ID = ?`
+    const params = [account_ID]
     db.query(sql, params, (err, result) => {
-      if (err) reject(err);
-      if (result.length) resolve(true);
-      else resolve(false);
-    });
-  });
-};
+      if (err) reject(err)
+      if (result.length) resolve(true)
+      else resolve(false)
+    })
+  })
+}
 const getNotification = async (account_ID) => {
   return new Promise((resolve, reject) => {
-    const sql = `SELECT * from announcement as a, has_announcment as h where a.ID = h.announcement_ID and h.employee_ID = ? `;
-    const params = [account_ID];
+    const sql = `SELECT * from announcement as a, has_announcment as h where a.ID = h.announcement_ID and h.employee_ID = ? `
+    const params = [account_ID]
     db.query(sql, params, (err, result) => {
-      if (err) reject(err);
-      if (result.length) resolve(result);
-      else resolve(false);
-    });
-  });
-};
+      if (err) reject(err)
+      if (result.length) resolve(result)
+      else resolve(false)
+    })
+  })
+}
 
 module.exports = {
-  updateUser,
+  updateInfo,
   changePassword,
   getUser,
   getOtp,
@@ -157,5 +157,5 @@ module.exports = {
   getEmployeeDetails,
   sendForm,
   getForm,
-  getNotification
-};
+  getNotification,
+}
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,15 +1,15 @@
-const express = require("express");
-const router = express.Router();
-const verifyToken = require("../middleware/authentication");
-const userController = require("../controller/userController");
+const express = require('express')
+const router = express.Router()
+const verifyToken = require('../middleware/authentication')
+const userController = require('../controller/userController')
 // router.get('/gen-otp', verifyToken, userController.generateOtp)
 // router.get('/verify-otp', verifyToken, userController.verifyOtp)
-router.post("/update", verifyToken, userController.updateUser);
-router.put("/changePassword", verifyToken, userController.changePassword);
-router.get("/attendanceTrack", verifyToken, userController.getAttendanceTrack);
-router.get("/employeeDetails", verifyToken, userController.getEmployeeDetails);
-router.post("/sendForm", verifyToken, userController.sendForm);
-router.get("/getForm", verifyToken, userController.getForm);
-router.get("/getNotification", verifyToken, userController.getNotification);
+router.patch('/update', verifyToken, userController.updateInfo)
+router.put('/changePassword', verifyToken, userController.changePassword)
+router.get('/attendanceTrack', verifyToken, userController.getAttendanceTrack)
+router.get('/employeeDetails', verifyToken, userController.getEmployeeDetails)
+router.post('/sendForm', verifyToken, userController.sendForm)
+router.get('/getForm', verifyToken, userController.getForm)
+router.get('/getNotification', verifyToken, userController.getNotification)
 
-module.exports = router;
+module.exports = router
